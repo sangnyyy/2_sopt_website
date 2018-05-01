6159e40272fabb54337a8b4b95626653befc932d
SML <dltkdals2202@gmail.com>
2018-05-02 01:31:16 +0900

    add : main background image of SOPT Homepage and import Titillium Web font from google api
    add img directory and main_image file and import Titillium Web font from google api.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <main-header></main-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import MainHeader from "./components/MainHeader"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainHeader
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
+  text-align: center;
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <img src="../../img/main_image.png">
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    header{
+        width : 100%;
+        height : 300px;
+    }
+    header > h1{
+        font-size: 200px;
+    }
+    header > img{
+        position: absolute;
+        top:0;
+        left:0;
+        width: 100%;
+        height : 100%;
+    }
+</style>
