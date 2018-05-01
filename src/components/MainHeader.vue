4e417e028c0e731791494611c629de3492cce557
SML <dltkdals2202@gmail.com>
2018-05-02 02:15:33 +0900

    fix : adjust position of main logo
    adjust position of main log SOPT.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,28 +1,35 @@
 <template>
-    <header>
+    <main>
         <img src="../../img/main_image.png">
-    </header>
+        <h1>SOPT</h1>
+    </main>
 </template>
 
 <script>
 export default {
-
-}
+    
+};
 </script>
 
 <style>
-    header{
-        width : 100%;
-        height : 300px;
-    }
-    header > h1{
-        font-size: 200px;
-    }
-    header > img{
-        position: absolute;
-        top:0;
-        left:0;
-        width: 100%;
-        height : 100%;
-    }
+main {
+  height: 100%;
+  text-align: center;
+}
+main > img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+main > h1 {
+  position: relative;
+  top : 35%;
+  transform: translateY(-50%);
+  font-size: 200px;
+  display: inline-block;
+  z-index: 2;
+}
 </style>
