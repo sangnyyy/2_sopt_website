c4300d2f7b81716756dd7f47b096a29179d6ff22
SML <dltkdals2202@gmail.com>
2018-05-03 03:13:17 +0900

    add and fix : add a lot of vue components and define router. apply transition when move to another page in router.
    add a lot of vue components and define router. apply transition when move to another page in router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <main-header></main-header>
+    <img src="../img/main_image.png">
+    <transition name="router-anim">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
 
-import MainHeader from "./components/MainHeader"
+import Home from "./components/Home"
+import Notice from "./components/Notice"
+import About from "./components/About"
+import Story from "./components/Story"
+import Faq from "./components/Faq"
+import Recruit from "./components/Recruit"
 
 export default {
   name: 'App',
   components: {
-    MainHeader
+    Home, Notice, About, Story, Faq, Recruit
   }
 }
 </script>
@@ -20,5 +28,42 @@ export default {
 #app {
   height: 100%;
   text-align: center;
+}
+#app img{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.router-anim-enter-active{
+  animation: coming 1.5s;
+  animation-delay: 1s;
+}
+.router-anim-leave-active{
+  animation: going 1.5s;
+}
+
+@keyframes going {
+  from{
+    transform: translateX(0);
+  }
+  to{
+    transform: translateX(-100px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from{
+    transform: translateX(-100px);
+    opacity: 0;
+  }
+  to{
+    transform: translateX(0);
+    opacity: 1;
+  }
 }
 </style>
