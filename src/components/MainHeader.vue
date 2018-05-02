b1f17a0abbb047829c092200df24e546979113b3
SML <dltkdals2202@gmail.com>
2018-05-02 14:54:09 +0900

    fix : adjust location of contents
    adjust location of contents.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,42 +1,41 @@
 <template>
     <main>
         <img src="../../img/main_image.png">
-        <transition appear appear-active-class="animated shake">
-            <h1>SOPT</h1>
-        </transition>
-        <br/>
-        
-        <transition appear appear-active-class="animated shake">
-            <h2>SHOUT OUR PASSION TOGETHER</h2>    
-        </transition>
-        <br>
-            <a href="#">
-                <transition appear appear-active-class="animated bounceInUp">
-                    <h3>
-                        About
-                    </h3>
-                </transition>
-            </a>
-            <a href="#">
-                <transition appear appear-active-class="animated bounceInUp">
-                    <h3>
-                        Story
-                    </h3>
-                </transition>
-            </a>
-            <a href="#">
-                <transition appear appear-active-class="animated bounceInUp">
-                    <h3>
-                        F.A.Q
-                    </h3>
-                </transition>
-            </a><a href="#">
-                <transition appear appear-active-class="animated bounceInUp">
-                    <h3>
-                        Recruit
-                    </h3>
-                </transition>
-            </a>
+        <div>
+            <transition appear appear-active-class="animated shake">
+                <h1>SOPT</h1>
+            </transition>
+            <transition appear appear-active-class="animated shake">
+                <h2>"SHOUT OUR PASSION TOGETHER"</h2>    
+            </transition>
+                <a href="#">
+                    <transition appear appear-active-class="animated bounceInUp">
+                        <h3>
+                            About
+                        </h3>
+                    </transition>
+                </a>
+                <a href="#">
+                    <transition appear appear-active-class="animated bounceInUp">
+                        <h3>
+                            Story
+                        </h3>
+                    </transition>
+                </a>
+                <a href="#">
+                    <transition appear appear-active-class="animated bounceInUp">
+                        <h3>
+                            F.A.Q
+                        </h3>
+                    </transition>
+                </a><a href="#">
+                    <transition appear appear-active-class="animated bounceInUp">
+                        <h3>
+                            Recruit
+                        </h3>
+                    </transition>
+                </a>
+            </div>
         
     </main>
 </template>
@@ -57,7 +56,9 @@ export default {
 <style>
 main {
   height: 100%;
-  text-align: center;
+  width: 100%;
+  top: 0;
+  left: 0;
 }
 main > img {
   position: absolute;
@@ -67,21 +68,25 @@ main > img {
   height: 100%;
   z-index: -1;
 }
+main div{
+    position: relative;
+    top: 15%;
+    display: inline-block;
+    text-align: center;
+}
 main h1 {
-  margin-top: 10%;
   color: white;
-  position: relative;
-  top: 20%;
-  /* transform: translateY(-50%); */
   font-size: 200px;
   display: inline-block;
+  height: 290px;
+
+  /* height: 250px; */
 }
 main h2 {
   color: white;
   font-size: 60px;
 }
 main a {
-  top: 15%;
   position: relative;
   color: white;
   text-decoration: none;
@@ -95,4 +100,6 @@ h3 {
 main a:hover {
   color: gray;
 }
+
+
 </style>
